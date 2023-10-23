ddcfbe933a2b020c0666bc7d895e891f2c881b0e
Jun Da <45486915+ngjunda@users.noreply.github.com>
2023-10-24 01:29:18 +0800

    Catalog module and service
    Catalog module and service

--- src/Web/WebSPA/Client/src/modules/catalog/catalog.module.ts
+++ src/Web/WebSPA/Client/src/modules/catalog/catalog.module.ts
@@ -1,17 +1,19 @@
-import { NgModule }             from '@angular/core';
-import { BrowserModule  }       from '@angular/platform-browser';
-import { CommonModule }         from '@angular/common'
-import { SharedModule }         from '../shared/shared.module';
-import { CatalogComponent }     from './catalog.component';
-import { CatalogService }       from './catalog.service';
-import { Pager }                from '../shared/components/pager/pager';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'
+import { SharedModule } from '../shared/shared.module';
+import { CatalogComponent } from './catalog.component';
+import { CatalogService } from './catalog.service';
+import { Pager } from '../shared/components/pager/pager';
 import { ManageProductComponent } from './manage-product/manage-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { SearchProductComponent } from './search-product/search-product.component';
 import { ViewProduct } from './view-product/view-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
-    imports: [BrowserModule, SharedModule, CommonModule],
+    imports: [BrowserModule, SharedModule, CommonModule, NgbModule],
     declarations: [CatalogComponent, ManageProductComponent, EditProductComponent, SearchProductComponent, ViewProduct],
     providers: [CatalogService]
 })
