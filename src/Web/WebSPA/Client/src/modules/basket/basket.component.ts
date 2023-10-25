94c178849beda6cbc49b4b9b0b0bf3136d7e8089
jwong93 <jwong93@hotmail.com>
2023-10-25 22:31:15 +0800

    Basket changes

--- src/Web/WebSPA/Client/src/modules/basket/basket.component.ts
+++ src/Web/WebSPA/Client/src/modules/basket/basket.component.ts
@@ -32,7 +32,6 @@ export class BasketComponent implements OnInit {
                 }
                 )
             }
-            console.log(basket.items);
             this.calculateTotalPrice();
         });
     }
